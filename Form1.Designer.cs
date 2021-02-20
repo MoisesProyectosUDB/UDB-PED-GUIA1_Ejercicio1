9d1cb733f21ad775792fbd7b02b71658dcbfd390
moises <CA140174@alumno.udb.edu.sv>
2021-02-20 17:09:16 -0600

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿
+namespace GuiaPractica1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.listBox1);
+            this.panel1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.panel1.Location = new System.Drawing.Point(56, 46);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(948, 749);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseClick);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "Circulo",
+            "Rectangulo",
+            "Curva",
+            "Poligono"});
+            this.listBox1.Location = new System.Drawing.Point(857, -1);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(86, 56);
+            this.listBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label1.Location = new System.Drawing.Point(164, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(276, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Toque la Pizzarra para que Aparescan Formas Diferentes";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1302, 835);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GuiaPractica1
+{
+    public partial class Form1 : Form
+    {
+        int x, y;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+            Graphics g = panel1.CreateGraphics();
+            Pen lapiz = new Pen(Color.White);
+
+            Random rdn = new Random(); //Instancia de Random    
+            int ValoraDibujar = rdn.Next(0, 8); //obtenemos el valor y el rango de los mismos
+            // Console.WriteLine("Valor Random" + ValoraDibujar);
+            int index = listBox1.Items.Count;
+            if (index <= ValoraDibujar)
+                //MessageBox.Show("Item is not available in ListBox2");
+                Console.WriteLine("Valor Random no Encontrado" + ValoraDibujar);
+               else
+               listBox1.SetSelected(ValoraDibujar, true);//Truco , seteamos el valor de la lsita para poder hacer la comparacnion
+
+            if (listBox1.SelectedIndex == ValoraDibujar)
+            {
+                //string textoLista = listBox1.Items[ValoraDibujar].ToString();
+                //Console.WriteLine("Valor Random" + ValoraBuscar + "Texto Lista" + textoLista);
+
+
+                if (listBox1.SelectedIndex == 0)
+                {
+                    SolidBrush sb = new SolidBrush(Color.Aqua);//Color
+                    g.DrawEllipse(lapiz, x - 50, y - 50, 250, 250);//Marco
+                    g.FillEllipse(sb, x - 50, y - 50, 250, 250);//Relleno
+
+
+                }
+                if (listBox1.SelectedIndex == 1)
+                {
+                    SolidBrush sb = new SolidBrush(Color.Brown);
+                    g.DrawRectangle(lapiz, x - 50, y - 50, 150, 150);
+                    g.FillRectangle(sb, x - 50, y - 50, 150, 150);
+
+                }
+                if (listBox1.SelectedIndex == 2)
+                {
+                    
+                    Point[] punto = { new Point(x-10, x-25), new Point(x - 10, x - 500), new Point(x - 80, x-75) };// se establecen los puntos  e igual que los demas se  toma como base el clic del mouse
+                    g.DrawCurve(Pens.Red , punto);
+                    
+                    
+
+                }
+                if (listBox1.SelectedIndex == 3)
+                {
+                   
+                    Point[] punto1 = { new Point(x - 30, x-45), new Point(x - 150, x - 400), new Point(x - 45, x - 40) };// se establecen los puntos  e igual que los demas se  toma como base el clic del mouse
+                    g.DrawPolygon(Pens.BlueViolet, punto1);
+                   
+
+                }
+
+
+            }
+          
+
+          
+
+
+
+        }
+
+        private void panel1_MouseClick(object sender, MouseEventArgs e)
+        {
+            Point punto = new Point(e.X, e.Y);
+            x = punto.X;//Posicion de X
+            y = punto.Y;//Posicion de Y
+            panel1.Invalidate();
+        }
+    }
+}
